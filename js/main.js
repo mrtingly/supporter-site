9595d22c201df000da6883cfa407560114337710
mrtingly <116870308+mrtingly@users.noreply.github.com>
2026-06-08 13:54:49 +0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,57 @@
+const logo = document.getElementById("ssbLogo");
+const menu = document.getElementById("menuPanel");
 
+let opened = false;
+
+logo.addEventListener("click", () => {
+
+    const sponsors = document.querySelectorAll(".supporter");
+
+    if (!opened) {
+
+        sponsors.forEach((item, index) => {
+
+            item.style.transition =
+            "all .8s ease";
+
+            item.style.transform =
+            "scale(0)";
+
+            item.style.opacity =
+            "0";
+
+        });
+
+        setTimeout(() => {
+            menu.classList.add("active");
+        }, 800);
+
+        opened = true;
+
+    }
+
+});
+
+menu.addEventListener("click", (e) => {
+
+    if (e.target === menu) {
+
+        menu.classList.remove("active");
+
+        const sponsors =
+        document.querySelectorAll(".supporter");
+
+        sponsors.forEach(item => {
+
+            item.style.transform =
+            "scale(1)";
+
+            item.style.opacity =
+            "1";
+
+        });
+
+        opened = false;
+    }
+
+});
